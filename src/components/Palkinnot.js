d219b54678940ef386bc5b03db2d1183256a8375
arkohonen <ar.kohonen@gmail.com>
2017-12-08 15:07:04 +0200

    palkintosivu ja palkintohaut lisätty, eka versio

--- src/components/Palkinnot.js
+++ src/components/Palkinnot.js
@@ -1,59 +1,52 @@
-import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View, StyleSheet, Image, TextInput, ScrollView} from 'react-native';
+import React, { Component } from 'react'
+import { ActivityIndicator, ListView, Text, View, StyleSheet, Image, TextInput, ScrollView } from 'react-native'
+import { haeAvoimetPalkinnot, haeLukitutPalkinnot } from '../tiedonhakusivut/Palkintohaku'
 
-
-export default class Palkinnot extends React.Component {
-    static navigationOptions = {
-        header: null,
-        showIcon: true,
-        tabBarIcon: ( {tintColor }) => {
-            return <Image
-                source={require('../kuvat/kirstu.png')}
-                style={{width: 26, height: 26, tintColor: tintColor}}
-            />;
-        }
-    };
-
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true
-        }
+export default class Palkinnot extends Component {
+  static navigationOptions = {
+    header: null,
+    showIcon: true,
+    tabBarIcon: ({tintColor}) => {
+      return <Image
+        source={require('../kuvat/kirstu.png')}
+        style={{width: 26, height: 26, tintColor: tintColor}}
+      />
     }
+  }
 
-    componentDidMount() {
-        return fetch('https://taskuri.herokuapp.com/kayttaja/kaikki')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-                this.setState({
-                    isLoading: false,
-                    dataSource: ds.cloneWithRows(responseJson),
-                });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+  state = {
+    avoimetPalkinnot: [],
+    lukitutPalkinnot: []
+  }
 
-    render() {
-        if (this.state.isLoading) {
-            return (
-                <View style={{flex: 1, paddingTop: 20}}>
-                    <ActivityIndicator />
-                </View>
-            );
-        }
+  componentDidMount () {
+    let konteksti = this
+    haeAvoimetPalkinnot(1)
+      .then(function (json) {
+        konteksti.setState({avoimetPalkinnot: [json]})
+      })
+    haeLukitutPalkinnot(1)
+      .then(function (json) {
+        konteksti.setState({lukitutPalkinnot: [json]})
+      })
+  }
 
+  render () {
+    let avoimetPalkinnot = this.state.avoimetPalkinnot.map(function (palkinto) {
+      return (
+        <Nimilistaus palkinto={palkinto} key={palkinto.id}/>
+      )
+    })
+      let lukitutPalkinnot = this.state.lukitutPalkinnot.map(function (palkinto) {
         return (
-            <View style={{flex: 1, paddingTop: 20}}>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData.nimi}, {rowData.salasana}, {rowData.rooli}, {rowData.pisteet}, {rowData.ryhmaID}, {rowData.email}, </Text>}
-                />
-            </View>
-        );
-    }
-
+          <Nimilistaus palkinto={palkinto} key={palkinto.id}/>
+        )
+      })
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <Nimilistaus sisalto={this.props.avoimetPalkinnot}/>
+          <Nimilistaus sisalto={this.props.lukitutPalkinnot}/>
+        </View>
+      )
+  }
 }
---
+++ src/tiedonhakusivut/Palkintohaku.js
@@ -0,0 +1,120 @@
+const palkintoUrl = 'https://taskuri.herokuapp.com/palkinto'
+
+export function haeAvoimetPalkinnot(kayttajaId) {
+  return fetch(palkintoUrl+ '/'+ kayttajaId + '/avoimet', {
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Ongelmia tiedonhaussa');
+  })
+}
+
+export function haeLukitutPalkinnot(kayttajaId) {
+  return fetch(palkintoUrl + '/' + kayttajaId + '/lukitut', {
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Ongelmia tiedonhaussa');
+    })
+}
+
+export function haeKaikkiPerheenPalkinnot(ryhmaId) {
+  return fetch(palkintoUrl+'/'+ryhmaId + '/kaikki', {
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Ongelmia tiedonhaussa');
+    })
+}
+
+export function haePerheenLunastetut(ryhmaId) {
+  return fetch(palkintoUrl+'/'+ryhmaId+'/lunastetut', {
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Ongelmia tiedonhaussa');
+    })
+}
+
+export function haeKayttajanLunastamat(kayttajaId) {
+  return fetch(palkintoUrl+'/lunastetut/' +kayttajaId, {
+  })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Ongelmia tiedonhaussa');
+    })
+}
+
+export function poistaPalkinto(palkintoId) {
+  return fetch(palkintoUrl+'/poista/'+palkintoId, {
+    method: 'DELETE',
+  })
+    .then((function(response) {
+      if (response.ok) {
+        return 'Muutokset onnistuivat';
+      } throw new Error('Muutokset epäonnistuivat')
+    }))
+}
+
+export function muokkaaPalkintoa(palkintoId) {
+  return fetch(palkintoUrl+'/muokkaa/'+palkintoId, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      'nimi': this.state.nimi,
+      'kuvaus': this.state.kuvaus,
+      'arvo': this.state.arvo
+    })
+  })
+    .then((function(response) {
+      if (response.ok) {
+        alert('Muutokset onnistuivat');
+      } throw new Error('Muutokset epäonnistuivat')
+    }))
+}
+
+export function lisaaPalkinto() {
+  return fetch(palkintoUrl+'/lisaa', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'nimi': this.state.nimi,
+      'kuvaus': this.state.kuvaus,
+      'arvo': this.state.arvo,
+      'ryhmaId': this.state.ryhmaId
+    })
+  })
+    .then((function(response) {
+      if (response.ok) {
+        alert('Palkinto lisätty');
+      } throw new Error('Lisäys ei onnistunut');
+    }));
+}
+
+export function lunastaPalkinto(kayttajaId, palkintoId) {
+  return fetch(palkintoUrl+'/'+ kayttajaId + '/lunasta/' + palkintoId, {
+    method: 'PUT',
+  })
+    .then((function(response) {
+      if (response.ok) {
+        alert('Muutokset onnistuivat');
+      } throw new Error('Muutokset epäonnistuivat')
+    }))
+}
+
+
