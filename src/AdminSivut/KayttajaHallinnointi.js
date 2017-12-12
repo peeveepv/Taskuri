98498a280dc629100fb3604dad6e2a33a1c9f681
Krristiina <32480997+Krristiina@users.noreply.github.com>
2017-12-12 14:43:16 +0200

    Add files via upload

---
+++ src/AdminSivut/KayttajaHallinnointi.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+const KayttajaHallinnointi = () => {
+    return (
+        <View style={styles.container}>
+            <Text
+                style={styles.welcome}
+                onPress={() => Actions.oletus()}>
+                Käyttäjien hallinnointisivu
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2382cc',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+    },
+});
+
+export default KayttajaHallinnointi;
