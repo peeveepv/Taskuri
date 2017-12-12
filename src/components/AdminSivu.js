9903ffb4b29a7889cace7f27d2b23e793098bbd9
arkohonen <ar.kohonen@gmail.com>
2017-12-12 16:24:15 +0200

    adminsivun vanha naviosuus poistettu

--- src/components/AdminSivu.js
+++ src/components/AdminSivu.js
@@ -7,16 +7,6 @@ import KayttajaHallinnointi from '../AdminSivut/KayttajaHallinnointi';
 import Oletusnakyma from '../AdminSivut/Oletusnakyma';
 
 export default class AdminSivu extends React.Component {
-    static navigationOptions = {
-        header: null,
-        showIcon: true,
-        tabBarIcon: ( {tintColor }) => {
-            return <Image
-                source={require('../kuvat/hammasratas.png')}
-                style={{width: 26, height: 26, tintColor: tintColor}}
-            />;
-        }
-    };
 
     render(){
         return(
