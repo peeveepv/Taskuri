2ead0537128300e2ab6b8201b8bad77c169555ac
Krristiina <32480997+Krristiina@users.noreply.github.com>
2017-12-12 13:40:36 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,17 @@
-import React, { Component } from 'react';
-import {TabNavigator,} from 'react-navigation';
-import Tehtavat from './src/components/Tehtavat';
-import Palkinnot from './src/components/Palkinnot';
-import AdminSivu from "./src/components/AdminSivu";
+import React from 'react';
+import Paanavigaattori from './src/components/Paanavigaattori'
+import {View} from 'react-native';
+
+//Tämä sivu pitää sisällään navigointiominaisuuden. Navigointipalkki näkyy puhelimen alareunassa.
 
 
-
-
-const MainScreenNavigator = TabNavigator({
-    Tehtävät : { screen: Tehtavat },
-    Palkinnot: {screen: Palkinnot },
-    Admin : {screen: AdminSivu},
-},
-{
-    tabBarPosition: 'bottom',
-    swipeEnabled: false,
-    tabBarOptions: {
-        activeTintColor: '#e91e63',
-        showIcon: true,
-    },
-    lazy: true,
-});
 
 
 export default class App extends React.Component {
     render() {
-        return <MainScreenNavigator/>
+        return (
+            <Paanavigaattori/>
+        )
+
     }
 }
