904a16304e8da970c2439ff7688e16339255515e
Krristiina <32480997+Krristiina@users.noreply.github.com>
2017-12-13 11:08:04 +0200

    Update Kayttajahaku.js

--- src/tiedonhakusivut/Kayttajahaku.js
+++ src/tiedonhakusivut/Kayttajahaku.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+//Tällä sivulla on metodeita, joilla haetaan tietoa käyttäjistä (kaikista tai vain yhdestä) sekä poistetaan/muokataan heidän tietojaan
+//Näitä metodeita käytetään pääasiassa AdminSivu.js-sivulla
+
 
     const palveluurl = 'https://taskuri.herokuapp.com/';
 
-    export function haeKaikkiKayttajat() {
-        return fetch(palveluurl+'/kayttaja/kaikki')
+    export function haeKaikkiRyhmanKayttajat(ryhmaId) {
+        return fetch(palveluurl+'/kayttaja/ryhma/' + ryhmaId)
             .then(function(response){
                 return response.json();
             })
@@ -24,17 +27,34 @@ import React, { Component } from 'react';
             })
     }
 
-    export function lisaaKayttaja(nimi, salasana, rooli, pisteet, ryhmaID, email) {
+    export function lisaaKayttaja(nimi, salasana, admin, pisteet, ryhmaID, email) {
         return fetch(palveluurl+'/kayttaja/lisaa', {
             method: 'POST',
             headers: {'Content-Type': 'application/json' },
-            body: JSON.stringify(nimi, salasana, rooli, pisteet, ryhmaID, email)
+            body: JSON.stringify({
+                "nimi": nimi,
+                "salasana": salasana,
+                "admin": admin,
+                "pisteet": pisteet,
+                "ryhmaID": ryhmaID,
+                "email": email,
+            })
         })
             .then((function(response) {
                 if (response.ok) {
                     return 'Lisätty';
                 } throw new Error('Lisäys ei onnistunut');
             }));
+    }
+
+    export function poistaKayttaja(id){
+        return fetch(palveluurl+'/kayttaja/poista/' + id,{
+        })
+            .then(function (response) {
+                if (response.ok){
+                    return 'Poisto onnistui';
+                } throw new Error ('Poisto ei onnistunut');
+            })
     }
 
 
